9da504b5bea937cc19faba9494914107c2da59c1
Daniel Choudhury <dannychoudhury@gmail.com>
2022-09-26 18:25:59 +0100

    Setup tailwind css 💄

---
+++ web/config/tailwind.config.js
@@ -0,0 +1,12 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    fontFamily: {
+      fancy: ['Space Grotesk', ...defaultTheme.fontFamily.sans],
+    },
+  },
+  plugins: [],
+}
