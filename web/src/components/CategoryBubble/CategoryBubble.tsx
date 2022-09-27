891eead57b1805c7c6581d0b4742107ef579e42a
Daniel Choudhury <dannychoudhury@gmail.com>
2022-09-27 12:14:48 +0100

    ui: A bit of finesse in category bubble

--- web/src/components/CategoryBubble/CategoryBubble.tsx
+++ web/src/components/CategoryBubble/CategoryBubble.tsx
@@ -9,11 +9,17 @@ const CategoryBubble = ({ imageUrl, name, onClick }: CategoryBubbleProps) => {
   return (
     <button
       onClick={onClick}
-      className="h-30 w-30 group relative flex rounded-full bg-orange-400 bg-center bg-no-repeat group-hover:shadow-xl xl:h-40 xl:w-40"
-      style={imageUrl && { backgroundImage: `url(${imageUrl})` }}
+      className="group relative flex h-32 w-32 rounded-full hover:shadow-xl lg:h-40 lg:w-40"
     >
-      <div className="absolute h-full w-full backdrop-brightness-50 backdrop-saturate-50 transition-all group-hover:backdrop-saturate-100"></div>
-      <h2 className="z-50 m-auto text-center text-lg text-white">{name}</h2>
+      <div
+        className="absolute h-full w-full rounded-full bg-orange-400 bg-center bg-no-repeat brightness-50 saturate-50 transition-all group-hover:brightness-75 group-hover:saturate-100"
+        style={imageUrl && { backgroundImage: `url(${imageUrl})` }}
+      />
+      <div className="m-auto brightness-100 saturate-100">
+        <h2 className="z-50 m-auto text-center text-lg font-light text-white">
+          {name}
+        </h2>
+      </div>
     </button>
   )
 }
