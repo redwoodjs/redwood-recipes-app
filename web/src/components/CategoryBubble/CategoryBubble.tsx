f4f465cd8e3aa319576a0e14bba8fd89f312b060
Daniel Choudhury <dannychoudhury@gmail.com>
2022-09-27 12:02:06 +0100

    ui: Fix CategoryBubble layout bug

--- web/src/components/CategoryBubble/CategoryBubble.tsx
+++ web/src/components/CategoryBubble/CategoryBubble.tsx
@@ -9,10 +9,10 @@ const CategoryBubble = ({ imageUrl, name, onClick }: CategoryBubbleProps) => {
   return (
     <button
       onClick={onClick}
-      className="group flex h-40 w-40 rounded-full bg-orange-400 bg-center bg-no-repeat group-hover:shadow-xl xl:h-56 xl:w-56"
+      className="h-30 w-30 group relative flex rounded-full bg-orange-400 bg-center bg-no-repeat group-hover:shadow-xl xl:h-40 xl:w-40"
       style={imageUrl && { backgroundImage: `url(${imageUrl})` }}
     >
-      <div className="fixed h-full w-full backdrop-brightness-50 backdrop-saturate-50 transition-all group-hover:backdrop-saturate-100"></div>
+      <div className="absolute h-full w-full backdrop-brightness-50 backdrop-saturate-50 transition-all group-hover:backdrop-saturate-100"></div>
       <h2 className="z-50 m-auto text-center text-lg text-white">{name}</h2>
     </button>
   )
