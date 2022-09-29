0ea9cbea4f39ceff4ff878327ad0ec83e9ec0684
Daniel Choudhury <dannychoudhury@gmail.com>
2022-09-29 12:14:05 +0100

    api: Add seed script with recipes

--- scripts/seed.ts
+++ scripts/seed.ts
@@ -1,3 +1,6 @@
+import fs from 'fs'
+import path from 'path'
+
 import type { Prisma } from '@prisma/client'
 import { db } from 'api/src/lib/db'
 
@@ -6,31 +9,27 @@ export default async () => {
     //
     // Manually seed via `yarn rw prisma db seed`
     // Seeds automatically with `yarn rw prisma migrate dev` and `yarn rw prisma migrate reset`
-    //
-    // Update "const data = []" to match your data model and seeding needs
-    //
-    const data: Prisma.UserExampleCreateArgs['data'][] = [
-      // To try this example data with the UserExample model in schema.prisma,
-      // uncomment the lines below and run 'yarn rw prisma migrate dev'
-      //
-      // { name: 'alice', email: 'alice@example.com' },
-      // { name: 'mark', email: 'mark@example.com' },
-      // { name: 'jackie', email: 'jackie@example.com' },
-      // { name: 'bob', email: 'bob@example.com' },
+
+    const data: Prisma.RecipeCreateArgs['data'][] = [
+      {
+        name: 'Baba Ganoush',
+        blurb: 'A delicious eggplant dip',
+        cuisine: 'Middle Eastern',
+        content: fs.readFileSync(
+          path.join(__dirname, './recipes/baba-ganoush.md'),
+          'utf-8'
+        ),
+        imageUrl:
+          'https://images.unsplash.com/photo-1558649332-07f21970d309?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
+      },
     ]
-    console.log(
-      "\nUsing the default './scripts/seed.{js,ts}' template\nEdit the file to add seed data\n"
-    )
 
     // Note: if using PostgreSQL, using `createMany` to insert multiple records is much faster
     // @see: https://www.prisma.io/docs/reference/api-reference/prisma-client-reference#createmany
     Promise.all(
-      //
-      // Change to match your data model and seeding needs
-      //
-      data.map(async (data: Prisma.UserExampleCreateArgs['data']) => {
-        const record = await db.userExample.create({ data })
-        console.log(record)
+      data.map(async (data) => {
+        const record = await db.recipe.create({ data })
+        console.log(`Seeded ${record.name}`)
       })
     )
 
