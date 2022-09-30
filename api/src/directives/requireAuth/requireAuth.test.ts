198fd4c7c37e362071ba179cd46385ea3efe19d0
Daniel Choudhury <dannychoudhury@gmail.com>
2022-09-30 11:02:55 +0100

    api: Fix requireAuth test after adding auth

--- api/src/directives/requireAuth/requireAuth.test.ts
+++ api/src/directives/requireAuth/requireAuth.test.ts
@@ -8,11 +8,20 @@ describe('requireAuth directive', () => {
     expect(getDirectiveName(requireAuth.schema)).toBe('requireAuth')
   })
 
-  it('requireAuth has stub implementation. Should not throw when current user', () => {
+  it('requireAuth does not throw when current user present', () => {
     // If you want to set values in context, pass it through e.g.
-    // mockRedwoodDirective(requireAuth, { context: { currentUser: { id: 1, name: 'Lebron McGretzky' } }})
-    const mockExecution = mockRedwoodDirective(requireAuth, { context: {} })
+    const mockExecution = mockRedwoodDirective(requireAuth, {
+      context: { currentUser: { id: 1 } },
+    })
 
     expect(mockExecution).not.toThrowError()
+  })
+
+  it('requireAuth throws if no currentUser present in context', () => {
+    const mockExecution = mockRedwoodDirective(requireAuth, {
+      context: {},
+    })
+
+    expect(mockExecution).toThrowError(`You don't have permission to do that`)
   })
 })
