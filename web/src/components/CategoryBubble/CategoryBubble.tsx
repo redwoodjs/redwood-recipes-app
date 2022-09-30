97364fe633bca5f492c69e99739992bd07540a5e
Daniel Choudhury <dannychoudhury@gmail.com>
2022-09-30 11:35:47 +0100

    ui: fix UI bug

--- web/src/components/CategoryBubble/CategoryBubble.tsx
+++ web/src/components/CategoryBubble/CategoryBubble.tsx
@@ -18,7 +18,7 @@ const CategoryBubble = ({
     >
       <div
         className="absolute h-full w-full rounded-full bg-orange-400 bg-center bg-no-repeat brightness-50 saturate-50 transition-all group-hover:brightness-75 group-hover:saturate-100"
-        style={imageUrl && { backgroundImage: `url(${imageUrl})` }}
+        style={imageUrl ? { backgroundImage: `url(${imageUrl})` } : {}}
       />
       <div className="m-auto brightness-100 saturate-100">
         <h2 className="z-50 m-auto text-center text-lg font-light text-white">
